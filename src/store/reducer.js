72fbc2918738f06b6783b78886462b9b999edbcc
chamikab <kasun.chamika@gmail.com>
2018-08-27 23:20:27 +0530

    Added changes to store updating values.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,25 +1,34 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 
 const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case 'INCREMENT':
-      return {
-        counter: state.counter + 1,
-      };
+      const newState = Object.assign({}, state); // One way of updating state immutably.
+      newState.counter = state.counter + 1;
+      return newState;
     case 'DECREMENT':
       return {
+        ...state,  // Second way of updating state immutably.
         counter: state.counter - 1,
       };
     case 'ADD':
       return {
+        ...state,
         counter: state.counter + action.val,
       };
     case 'SUBTRACT':
       return {
+        ...state,
         counter: state.counter - action.val,
+      };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: +new Date(),value: state.counter}), // Here we are using concat as in the push method it updates values mutably.
       };
     default:
       return state;
