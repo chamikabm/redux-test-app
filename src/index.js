c274dd2ae75b8fc3853e4ecf56d42dd03a32a76c
chamikab <kasun.chamika@gmail.com>
2018-08-31 23:39:05 +0530

    Added debug support for the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -18,6 +18,7 @@ const logger = store => {
   return next => {
     return action => {
       console.log('[Middleware] Dispatching', action);
+      console.log('[Middleware] Next State', store.getState());
       const result = next(action);
       console.log('[Middleware] Next State', store.getState());
       return result;
@@ -25,7 +26,9 @@ const logger = store => {
   }
 };
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
